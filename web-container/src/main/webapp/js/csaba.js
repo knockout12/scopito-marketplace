5a255773e1e4b4a8bc0ad138f04b33b814fe4a2b
Csaba <csabaamann@gmail.com>
2017-05-18 11:22:51 +0200

    Updating Thomas branch

---
+++ web-container/src/main/webapp/js/csaba.js
@@ -0,0 +1,49 @@
+jQuery.noConflict();
+jQuery(document).ready(function(){
+
+
+    jQuery(document).ready(function(){
+
+        jQuery("#grid-btn").addClass("active");
+        jQuery("#list-btn").removeClass("active");
+        jQuery("#list-view").hide();
+        jQuery("#grid-view").show();
+        jQuery("#welcome-modal").modal;
+
+    })
+
+
+
+    //Makes the advanced search arrow rotate
+    //Not using toggle method because sometimes rotation was mismatched with the view
+
+    jQuery("#chevron").click(function(){
+        if(jQuery("#advanced-search").hasClass("in")){
+            jQuery(this).removeClass("rotate-minus-90");
+            jQuery(this).addClass("rotate-90");
+        }
+
+        else{
+            jQuery(this).removeClass("rotate-90");
+            jQuery(this).addClass("rotate-minus-90");
+        }
+    });
+
+    //Grid and list view button functionality
+
+    jQuery("#grid-btn").click(function(){
+        jQuery("#grid-btn").addClass("active");
+        jQuery("#list-btn").removeClass("active");
+        jQuery("#list-view").hide();
+        jQuery("#grid-view").show();
+
+    });
+
+    jQuery("#list-btn").click(function(){
+        jQuery("#list-btn").addClass("active");
+        jQuery("#grid-btn").removeClass("active");
+        jQuery("#grid-view").hide();
+        jQuery("#list-view").show();
+    });
+
+});
